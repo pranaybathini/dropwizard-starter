e437161589963577be610bd00c9b4cbb1406550a
Pranay Bathini <pranaygoud36@gmail.com>
2022-12-30 19:13:30 +0530

    DropWizard Starter Skeleton
    Feel Free to Download and build

---
+++ src/main/java/com/pranaybathini/DropWizardConfiguration.java
@@ -0,0 +1,10 @@
+package com.pranaybathini;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class DropWizardConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
